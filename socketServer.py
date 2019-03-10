4a7c2f2523b0d280e89468b0f8817982997264bc
fgsrfug <33363369+fgsrfug@users.noreply.github.com>
2019-03-10 13:49:55 -0700

    Checkpoint save

--- socketServer.py
+++ socketServer.py
@@ -2,19 +2,64 @@
 
 import socket
 import time
+import subprocess
+
+#image = 'chungus.jpg'
+
+#-----------------------------------------------------------------------
+#Function used to send the image
+def sendImage():
+    #Create object to hold image
+    sampleImage = 'sampleImage.jpg'
+    #open the image for reading
+    readImage = open(sampleImage, 'rb')
+    #Get the image bytes
+    imageBytes = readImage.read()
+    #Get the number of bytes
+    numBytes = len(fileBytes)
+    #Create string to print out numBytes
+    sNumBytes = "%d" % numBytes
+    print(numBytes)
+    print(sNumBytes)
+    
+    #While there are still bytes to send
+    while (fileBytes):
+        #Send the bytes
+        conn.sendall(fileBytes)
+        fileBytes = myfile.read()
+        print("Sent file")
+#-----------------------------------------------------------------------
+
+#-----------------------------------------------------------------------
+#Function to send strings to the client
+#This probably doesn't need to be function, but I'm doing this because Spencer
+#will give me shit if I dont "mOdUlErIzE mY pRoGrAmS"
+def sendString(str):
+    #Call sendall to forward the string
+    conn.sendall(str)
+#-----------------------------------------------------------------------
+
+#-----------------------------------------------------------------------
+#Function to close the socket
+#This also probably doesn't need to be a function, but whatevs
+def closeSocket():
+    print("About to close socket")
+    conn.close()
+    print("closing socket")
+#-----------------------------------------------------------------------
 
 #Create variables for host and port. Leave host blank so as to listen listen for connections on port 9090
 host = ''
 port = 9090
 
-image = 'luigi.jpeg'
-    
 #Debugging print statement and message to be sent to the client
 print("Beginning socket communication")
 message = "giddy-up\n"
 cMessage = ""
 sNumBytes = ""
 #while True:
+
+subprocess.call("./laserControl.py", shell=True) 
 
 #Create the socket to be a standart TCPstream called serverSocket
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serverSocket:
@@ -40,39 +85,39 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serverSocket:
         while True:
             print('top of while loop')
             print(data)
-            data = conn.recv(1024)
+            cMessage = conn.recv(1024)
             
-            cMessage = data
-            #if (data == cMessage):
-            #    print('should close connection')
-            #    break
-            #Break if we didn't get anything
-            print("Message from server: " + repr(data))
-            if not data:
-                print('breaking to close')
+            #Print the client message for testing purposes
+            print("Message from server: " + repr(cMessage))
+            
+            #If cMessage is empty, close the socket
+            if not cMessage:
+                closeSocket()
                 break
             
+            #Check the client message to determine next action
+            if (cMessage == "This is the Client"):
+                #Call laserControl to turn on laser
+                subprocess.call("./laserControl.py", shell=True) 
+                print('calling laserControl.py and camera.sh')
+                #Send the image to the client
+                sendImage()
+                #Wait 5 seconds before sending data to the client so they have
+                #time to look at the pretty colors that is the image
+                time.sleep(5)
+                #Send supporting data to accompany the image
+                sendString(str)
+            
+            if (cMessage == "close"):
+                #Call close socket
+                closeSocket()
+
             #print('below data recv')
             
             #Print the data to the terminal
-            
-            myfile = open(image, 'rb')
-            fileBytes = myfile.read()
-            numBytes = len(fileBytes)
-            sNumBytes = "%d" % numBytes
-            print(numBytes)
-            print(sNumBytes)
-             
-            while (fileBytes):
-                conn.sendall(fileBytes)
-                fileBytes = myfile.read()
-                print("Sent file")
             #Send our message out to the client
             #conn.sendto(message.encode(),(host,port))
             #conn.sendall(data)
         
-        print("About to close socket")
-        conn.close()
-        print("closing socket")
       #  time.sleep(60)
       #  print("slept for 60 seconds")
