b26f7e5e79ef81cb7765297f6ecb2cfa6f505db1
fgsrfug <33363369+fgsrfug@users.noreply.github.com>
2019-04-06 14:46:35 -0700

    Commented camera.sh

--- camera/camera.sh
+++ camera/camera.sh
@@ -2,4 +2,11 @@
 
 DATE=$(date +"%Y-%m-%d_%H%M")
 
-raspistill -ex verylong -o /home/pi/camera/$DATE.jpg
+#raspistill -ex verylong -o /home/pi/camera/$DATE.jpg
+#Exposure (ex) = very long
+#Width of image (w) = 640 pixels
+#Height of image (h) = 480 pixels
+#Quality of image (q) = 50 (The lower the number, the more compressed the image is)
+#Preview (t) = .5 second preview before image is taken
+#Sharpness (sh) = Sharpest image can be
+raspistill -ex verylong -w 640 -h 480 -q 50 -t 500 -sh 100 -o /home/pi/piControl/sampleImage.jpg
