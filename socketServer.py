68329cb489473fb10ec66dea79c914f7a55f9e5e
fgsrfug <33363369+fgsrfug@users.noreply.github.com>
2019-05-01 15:21:09 -0700

    Version 1.1 - Continuous operation

--- socketServer.py
+++ socketServer.py
@@ -8,7 +8,7 @@ import subprocess
 messageReceived = "Message received\n"
 cMessage = ""
 
-image = 'test_images/luigi.jpg'
+image = 'test_images/sample.jpg'
 
 #-----------------------------------------------------------------------
 #Function used to send the image
@@ -85,92 +85,94 @@ def getImageOutputs():
     print(sOutput)
     return sOutput
 #-----------------------------------------------------------------------
+while True:
+    socketConnected = False        
+    if (~socketConnected):
+        #Create variables for host and port. Leave host blank so as to listen listen for connections on port 9090
+        host = ''
+        port = 9090
 
-#Create variables for host and port. Leave host blank so as to listen listen for connections on port 9090
-host = ''
-port = 9090
+        #Debugging print statement and message to be sent to the client
+        print("Beginning socket communication")
 
-#Debugging print statement and message to be sent to the client
-print("Beginning socket communication")
-#getImageOutputs()
-
-#Create the socket to be a standart TCPstream called serverSocket
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serverSocket:
-    
-    #Bind to port 9090
-    serverSocket.bind((host, port))
-    
-    #Listen for any connections on the port 
-    serverSocket.listen()
-    
-    #Assign the return values of accept() to conn and addr
-    #conn is a socket object 
-    conn, addr = serverSocket.accept()
-    
-    with conn:
-        
-        #Tell the user that we have been connected to 
-        print('Connected by', addr)
-        data = ''
-        
-
-        #While we're connected, wait for data to come in
-        while True:
-            print('Beginning action!')
-            #Recieve our message from the client
-            cMessage = conn.recv(1024)
-            cMessage = cMessage.decode("utf-8") 
-            cMessage = cMessage.replace('\n','')
-            #Print the client message for testing purposes
-            print("Message from client: " + repr(cMessage))
+        #Create the socket to be a standart TCPstream called serverSocket
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serverSocket:
             
-            #Check the client message to determine next action
+            #Bind to port 9090
+            serverSocket.bind((host, port))
             
-            #If cMessage is empty, close the socket
-            if not cMessage:
-                closeSocket()
-                break
+            #Listen for any connections on the port 
+            serverSocket.listen()
             
-            elif (cMessage == "This is the Client"):
-                print("Matched with this is the client")
-                sendString(messageReceived)
-
-            elif ("Laser intensity" in cMessage):
-                print("Matched with laser intensity")
-                sendString(messageReceived)
+            #Assign the return values of accept() to conn and addr
+            #conn is a socket object 
+            conn, addr = serverSocket.accept()
             
-            elif ("Camera exposure" in cMessage):
-                print("Matched with camera exposure")
-                sendString(messageReceived)
+            with conn:
+                
+                #Tell the user that we have been connected to 
+                print('Connected by', addr)
+                data = ''
+                socketConnected = True        
 
-            elif ("Send image size" in cMessage):
-                print("Matched with send image size, taking picture")
-                #Call laserControl to turn on laser
-                #subprocess.call("./laserControl.py", shell=True) 
-                imageBytes = createImageObject() 
-                print('calling laserControl.py and camera.sh')
-                sendImageSize(imageBytes)
+                #While we're connected, wait for data to come in
+                while socketConnected:
+                    print('Beginning action!')
+                    #Recieve our message from the client
+                    cMessage = conn.recv(1024)
+                    cMessage = cMessage.decode("utf-8") 
+                    cMessage = cMessage.replace('\n','')
+                    #Print the client message for testing purposes
+                    print("Message from client: " + repr(cMessage))
+                    
+                    #Check the client message to determine next action
+                    
+                    #If cMessage is empty, close the socket
+                    if not cMessage:
+                        closeSocket()
+                        break
+                    
+                    elif (cMessage == "This is the Client"):
+                        print("Matched with this is the client")
+                        sendString(messageReceived)
 
-            #Check for first message to initiate analysis
-            elif (cMessage == "Ready for image"):
-                print ("Initiating Analysis")
-                #Send the image to the client 
-                #sendImage(imageBytes)
-                #Wait 5 seconds before sending data to the client so they have
-                #time to look at the pretty colors that is the image
-                #print ("going to sleep")
-                #time.sleep(5)
-           
-           #Once the client has recieved the image, client sends ACK for us to
-           #send the image outputs
-            elif(cMessage == "Got image"):
-                print ("Sending sample concentration")
-                #Send supporting data to accompany the image
-                #getImageOutputs()
-                sendString(getImageOutputs())
+                    elif ("Laser intensity" in cMessage):
+                        print("Matched with laser intensity")
+                        sendString(messageReceived)
+                    
+                    elif ("Camera exposure" in cMessage):
+                        print("Matched with camera exposure")
+                        sendString(messageReceived)
 
-            #Once the client is done, check if they want to close the socket
-            elif (cMessage == "close"):
-                #Call close socket
-                closeSocket()
+                    elif ("Send image size" in cMessage):
+                        print("Matched with send image size, taking picture")
+                        #Call laserControl to turn on laser
+                        subprocess.call("./laserControl.py", shell=True) 
+                        imageBytes = createImageObject() 
+                        print('calling laserControl.py and camera.sh')
+                        sendImageSize(imageBytes)
+                   
+                   #Once the client has recieved the image, client sends ACK for us to
+                   #send the image outputs
+                    elif(cMessage == "Send image attributes"):
+                        print ("Sending sample concentration")
+                        #Send supporting data to accompany the image
+                        getImageOutputs()
+                        sendString(getImageOutputs())
+
+                    #Check for first message to initiate analysis
+                    elif (cMessage == "Ready for image"):
+                        print ("Initiating Analysis")
+                        #Send the image to the client 
+                        sendImage(imageBytes)
+                        #Wait 5 seconds before sending data to the client so they have
+                        #time to look at the pretty colors that is the image
+                        #print ("going to sleep")
+                        #time.sleep(5)
+                   
+                    #Once the client is done, check if they want to close the socket
+                    elif (cMessage == "close"):
+                        #Call close socket
+                        closeSocket()
+                        socketConnected = False
 
