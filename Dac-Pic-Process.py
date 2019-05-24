dea097945917945f8e8dd7665aa99817aca0d102
parkerco35 <parkerco@oregonstate.edu>
2019-05-24 01:03:27 -0700

    Update Dac-Pic-Process.py

--- Dac-Pic-Process.py
+++ Dac-Pic-Process.py
@@ -1,59 +1,86 @@
 #!/usr/bin/env python2.7
-from PIL import Image 
+from PIL import Image, ImageEnhance
 import subprocess
 import time
 import string
-import Adafruit_MCP4725
+import Adafruit_MCP4725 
+import numpy
 
-concentration = 0
 dac = Adafruit_MCP4725.MCP4725()
 
-#Set the voltage
-def setVoltage():
-
-	dac.set_voltage(3195)#set to 3.9V
+concentration = 0
+total =0
 
 #Camera takes picture and saves it in piControl
 def takePic():
 	subprocess.call('./camera.sh', shell=True)
-	print 'taking picture' 
+
+def transFunc(total):
+    global concentration
+    if total < 3150:
+        concentration = 0
+    if total > 3150 and total < 3307:#0-5%
+        concentration = 3.168567807*10**(-2)*total - 99.8225602
+    if total > 3307 and total < 4145:#5-10%
+        concentration = 5.973715651*10**(-3)*total - 14.76224612
+    if total > 4144 and total < 4987:#10-20%
+        concentration = 1.18807176*10**(-2)*total - 39.24795058
+    if total > 4986 and total < 5832:#20-40%
+        concentration = 2.361832782*10**(-2)*total - 97.78223902
+    if total > 5832 and total < 7912:#40-60%
+        concentration = 9.620472365*10**(-3)*total - 16.12294964
+    if total > 7911 and total < 8910:#60-80%
+        concentration =2.510040161*10**(-2)*total - 138.6094378
+    if total > 8909 and total < 9762:#80-100%
+        concentration = 1.901140684*10**(-2)*total - 85.57794677
+    if total > 9761:
+        concentration = 100
+
 
 #process image based on laser intensity
 def processImage():
-	global concentration
+	global total
 	#read in image
-	img = Image.open('sampleImage.jpg')
-	total = 0
-	
-	for x in range(1500,1900):
-		for y in range(1050,1450):
+	img = Image.open('sampleImage.png')
+        total = 0
+	for x in range(220,420):
+		for y in range(150,350):
 			pixel = img.getpixel((x,y))			
-			if pixel[1] > 5 and pixel[1] > pixel[2] and pixel[1] > pixel[0]:
-				total = total + 1
-                                g = pixel[1] + 25
-                                r = pixel[0]
-                                b = pixel[2]
-                                img.putpixel((x,y),(r,g,b))
-				
-	#check if in correct laser range and calculate concentration-linear piecewise
-	if total > 30890 and total < 39929:#0-5%
-		con = (5.532813253*10**(-4))*total - 17.09159952
-	elif total > 39928 and total < 58919:#5-10%
-		con = (2.39343316*10**(-4))*total - 4.101528993
-	elif total > 58918 and total < 66102:#10-20%
-		con = (1.296494774*10**(-3))*total - 65.7005333
-	elif total > 66101 and total < 84339:#20-40%
-		con = (1.083195821*10**(-3))*total - 51.35540802
-	elif total > 84338 and total < 108129:#40-60%
-		con = (8.303536561*10**(-4))*total - 29.78508461
-	elif total > 108128 and total < 138525:#60-80%
-		con = (6.486892884*10**(-4))*total - 9.858840917
-	
-setVoltage()#turn on laser to set level
-takePic()
-processImage()
-print str(concentration)
-image = Image.open('sampleImage.jpg')
-image = image.crop((1100,800,2200,1700))#crop it to important area
-image.save('sampleImage.jpg')#save final image
-dac.set_voltage(0)#set to 0V
+                        if pixel[1] > 1 and pixel[1] > pixel[2] and pixel[1] > pixel[0]:
+                                total = total + 1
+        #print 'total is ' +str(total)
+        img.save('sampleImage.png')#save final image
+
+samples = [0,0,0]
+for x in range(0,3):
+    dac.set_voltage(3195)#set to 3.9V
+    takePic()
+    dac.set_voltage(0)#set to 0V
+    processImage()    
+    samples[x] = total
+    
+image = Image.open('sampleImage.png')
+image = image.crop((220,150,420,350))#crop it to important area
+enhancer = ImageEnhance.Brightness(image)
+image = enhancer.enhance(1.8)
+image.save('sampleImage.png')#save final image
+
+nump_samp = numpy.array(samples)
+maxArea = numpy.min(nump_samp, axis=0) + 700
+for x in numpy.nditer(nump_samp):
+    if x > maxArea:
+            nump_samp = numpy.delete(nump_samp, numpy.where(nump_samp == x), axis=0)
+
+meanArea = numpy.mean(nump_samp, axis=0)
+#print(nump_samp)
+#print 'Here is the mean area ' +str(meanArea)
+transFunc(meanArea)
+print 'Concentration is ' +str(round(concentration,4)) + '%'
+
+
+
+
+
+
+
+
